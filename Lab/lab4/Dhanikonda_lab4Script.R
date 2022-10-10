7ed0f52d5326334797b299cca35515602293cdb0
“Nagendra <dhanikonda@gmail.com>
2022-10-09 21:35:55 -0400

    Comments describing vector LEEs role in for loop addded to Dhanikonda_lab4Script.R

--- Lab/lab4/Dhanikonda_lab4Script.R
+++ Lab/lab4/Dhanikonda_lab4Script.R
@@ -2,15 +2,16 @@
 filename <- readline(prompt="Enter file path: ")
 #upload user input as dataframe
 filename <- read.csv(filename,header=TRUE)
-#create vector for Paper ID column 
+#create vector named LEEs for Paper ID column. 
 LEEs <- filename$Paper.ID
-#substitute "[" "]" "'" and space symbols with "nothing"
+#substitute "[" "]" "'" and space symbols with "nothing" in the data frame.
 filename$Paper.ID <- gsub("\\]", "", filename$Paper.ID)
 filename$Paper.ID <- gsub("\\[", "", filename$Paper.ID)
 filename$Paper.ID <- gsub("\\'", "", filename$Paper.ID)
 filename$Paper.ID <- gsub(" ", "", filename$Paper.ID)
+#c(length(LEEs)) used to correspond to a row in the dataframe in each iteration of the for loop
 #vector of strings is split into individual characters with strsplit. Unlist used to revectorize the list. 
-#number of unique paper ID was found for each row and row was deleted if the unique values = 1
+#number of unique paper IDs was found for each row and row was deleted if the unique values = 1
 for (each in c(length(LEEs):1)){
   if (length(unique(unlist(strsplit(filename$Paper.ID[each], ",")))) == 1){
     filename <- filename[-c(each),]
