7ecfe562b98bba5b034b93adeb0c5a9c8435e023
Rowan Angstadt <e.rowan.angstadt@gmail.com>
2022-11-15 11:02:31 -0500

    update for buddy debug

---
+++ Graphs.R
@@ -0,0 +1,41 @@
+# graphs 
+# using BLASTCompare and TxHist dataframes from FinalSequenceSort.Rmd 
+
+# setup 
+library(ggplot2) 
+
+# density plot of similarity values 
+ggplot(BLASTCompare) + geom_density(aes(x = SimilarityPct))
+
+# relationship between similarity and study length 
+ggplot(BLASTCompare) + geom_point(aes(x = EndDayNumber, y = SimilarityPct))
+
+# BP length 
+ggplot(BLASTCompare) + geom_histogram(aes(x = BPlength))
+
+# BP length and similarity 
+ggplot(BLASTCompare) + geom_point(aes(x = BPlength, y = SimilarityPct))
+
+# tx history (type) and similarity pct 
+# first, split up by tx type 
+PIdata <- BLASTCompare[which(BLASTCompare$PI == 1),]
+NRTIdata <- BLASTCompare[which(BLASTCompare$NRTI == 1),]
+NNRTIdata <- BLASTCompare[which(BLASTCompare$NNRTI == 1),]
+INIdata <- BLASTCompare[which(BLASTCompare$INI == 1),]
+# no patients for INSTI   
+
+# libraries for putting ggplots together on one page 
+library(gridExtra) 
+library(ggpubr)
+
+# make all the graphs 
+PIplot <- ggplot(PIdata) + geom_density(aes(x = SimilarityPct)) 
+NRTIplot <- ggplot(NRTIdata) + geom_density(aes(x = SimilarityPct)) 
+NNRTIplot <- ggplot(NNRTIdata) + geom_density(aes(x = SimilarityPct)) 
+INIplot <- ggplot(INIdata) + geom_density(aes(x = SimilarityPct)) 
+
+# display graphs together 
+ggarrange(PIplot, NRTIplot, NNRTIplot, INIplot, labels = c(), ncol = 2, nrow = 2)
+
+
+
